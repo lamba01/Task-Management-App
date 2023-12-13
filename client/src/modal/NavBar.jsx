a6e53002042488fb2ade54578dabc68281625fad
lamba01 <jamesspaul987@gmail.com>
2023-12-13 09:39:31 +0100

    adding responsiveness to topnav

--- client/src/modal/NavBar.jsx
+++ client/src/modal/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { SlOptionsVertical } from "react-icons/sl"
 import './navbar.css'
 import logo from "./images/logo-light.svg"
+import mobilelogo from "./images/logo-mobile.svg"
 
 const NavBar = () => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -13,13 +14,14 @@ const NavBar = () => {
     <nav className="navbar">
     <div className="navbar-container containerr">
       <input type="checkbox" name="" id="" />
-      <div className="hamburger-lines">
+      {/* <div className="hamburger-lines">
         <span className="line line1"></span>
         <span className="line line2"></span>
         <span className="line line3"></span>
-      </div>
+      </div> */}
       <ul className ="menu-items">
       <button className='add-task-btn'>+Add new task</button> 
+      <button className='add-task-mobile'>+</button> 
       <li onClick={toggleDropdown}><SlOptionsVertical /></li>
             {dropdownVisible && (
               <div className="dropdown-content">
@@ -30,7 +32,8 @@ const NavBar = () => {
       
       </ul>
       <h1 className='board-name'>test</h1>
-     <img className ="logo" src={logo} alt="" />  
+     <img className ="logo" src={logo} alt="logo" /> 
+     <img className='mobile-logo' src={mobilelogo} alt="mobile-logo" />
     </div>
   </nav>
   );
