47d65af26315078e9949442b11db34b69759fb98
lamba01 <jamesspaul987@gmail.com>
2023-12-13 12:03:35 +0100

    made header and sidebar responsive

--- client/src/modal/SideBar.jsx
+++ client/src/modal/SideBar.jsx
@@ -3,8 +3,11 @@ import "./sidebar.css"
 
 function SideBar() {
   return (
+    <div className="side">
+    <div className="overlay"></div>
     <div className='sidebar'>
       <h6 className='all-boards'>all boards</h6>
+      </div>
     </div>
   )
 }
