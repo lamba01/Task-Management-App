2f1bbf0ab4a55de97da64c0b7c120d5513a23d69
lamba01 <jamesspaul987@gmail.com>
2025-03-24 19:16:32 +0100

    fix server error

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -21,7 +21,7 @@ async function forgotPassword(req, res) {
 
     // Update password
     db.query(
-      "UPDATE user SET password = ? WHERE user_id = ?",
+      "UPDATE users SET password = ? WHERE user_id = ?",
       [hashedPassword, decoded.id],
       (err, result) => {
         if (err) {
